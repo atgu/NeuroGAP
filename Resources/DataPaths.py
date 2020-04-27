fc104b2822c152567b1ef38ee67a9bf7947819bf
Zan Koenig <ank.koenig@gmail.com>
2020-04-27 17:16:34 -0400

    initial commit for NeuroGAP data pathways

---
+++ Resources/DataPaths.py
@@ -0,0 +1,51 @@
+# Pilot data VCF paths
+kenyaMoi_vcf = "gs://fc-087888c3-eb94-4828-9926-d8d713eef821/NeuroGap_Koenen_Atwoli_Kenya-MoiPilot_GSA-MD_v1" \
+          "/NeuroGap_Koenen_Atwoli_Kenya-MoiPilot_GSA-MD_v1.vcf.gz "
+
+uganda_vcf = "gs://fc-cd517a08-fbb2-451f-a104-d21b32c3663c/NeuroGap_Koenen_Akena_UgandaPilot_GSA-MD_v1" \
+          "/NeuroGap_Koenen_Akena_UgandaPilot_GSA-MD_v1.vcf.gz "
+
+kenyaKemri_vcf = "gs://fc-abcbfa13-e1ff-4edb-bbb9-8ad757b3f2e0/NeuroGap_Koenen_Newton__Kenya-KEMRI_GSA-MD" \
+          "/NeuroGap_Koenen_Newton__Kenya-KEMRI_GSA-MD.vcf.gz "
+
+ethiopia_vcf = "gs://fc-8b77bd6a-19a6-42aa-9670-867fbd768b74/NeuroGap_Koenen_Teferra_Ethiopia_GSA-MD" \
+          "/NeuroGap_Koenen_Teferra_Ethiopia_GSA-MD.vcf.gz "
+
+southAfrica_vcf = "gs://fc-b2479a12-eaf2-4f96-891f-920a0191c252/NeuroGap_Koenen_Stein_SouthAfrica_GSA-MD" \
+          "/NeuroGap_Koenen_Stein_SouthAfrica_GSA-MD.vcf.gz "
+
+
+# Post QC NeuroGAP pilot data
+ngap_clean_vcf_cloud = "gs://neurogap-pilot-clean/NeuroGAP_pilotData_clean.vcf.bgz"
+
+ngap_clean_vcf_cloud = "gs://neurogap-pilot-clean/NeuroGAP_pilotData_clean.mt"
+
+ngap_clean_bed_cloud = "gs://neurogap-pilot-clean/NeuroGAP_pilotData_clean.bed"
+
+ngap_clean_bim_cloud = "gs://neurogap-pilot-clean/NeuroGAP_pilotData_clean.bim"
+
+ngap_clean_fam_cloud = "gs://neurogap-pilot-clean/NeuroGAP_pilotData_clean.fam"
+
+
+# Post QC NeuroGAP pilot + Nigerian data on cloud
+ngap_nga_clean_vcf_cloud = "gs://neurogap-pilot-clean/NeuroGAP_pilotNigeria_merge.vcf.bgz"
+
+ngap_nga_clean_mt_cloud = "gs://neurogap-pilot-clean/NeuroGAP_pilotNigeria_merge.mt"
+
+ngap_nga_clean_bed_cloud = "gs://neurogap-pilot-clean/NeuroGAP_pilotNigeria_merge.bed"
+
+ngap_nga_clean_bim_cloud = "gs://neurogap-pilot-clean/NeuroGAP_pilotNigeria_merge.bim"
+
+ngap_nga_clean_fam_cloud = "gs://neurogap-pilot-clean/NeuroGAP_pilotNigeria_merge.fam"
+
+
+# Post QC NeuroGAP pilot + Nigerian data on server
+
+ngap_nga_clean_bed_server = "/stanley/genetics/global/neurogap/data/pilot_plates/postQC/NeuroGAP_pilotNigeria_merge.bed"
+
+ngap_nga_clean_bim_server = "/stanley/genetics/global/neurogap/data/pilot_plates/postQC/NeuroGAP_pilotNigeria_merge.bim"
+
+ngap_nga_clean_fam_server = "/stanley/genetics/global/neurogap/data/pilot_plates/postQC/NeuroGAP_pilotNigeria_merge.fam"
+
+
+# Reference Data
