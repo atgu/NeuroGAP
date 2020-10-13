660a78b50eee7bc4adc0ec8ff93f19e693a577be
LindoNkambule <lindonkambule116@gmail.com>
2020-10-13 21:37:02 +0200

    updated getSampleNames.py
    the MergeVCFs function in picard has an issue handling file names with special characters such as #, so we had to replace those with _

--- african_refseq_processing/getSampleNames.py
+++ african_refseq_processing/getSampleNames.py
@@ -11,6 +11,8 @@ with open('inputSamplesFile.txt', 'w') as f:
             base = os.path.basename(file_path) # get file name without path
             # get file name without .{bam,cram} extension: for ubam file
             file_no_ext = os.path.splitext(base)[0]
+            file_no_ext = file_ext.replace("#", "_") # replace the # with _ because MergeVCFs in picard has a trouble dealing with special characters like #
+            # e.g before this change, one of the file names was EGAR00001140731_10256_1#73 and will be changed to EGAR00001140731_10256_1_73
             sample = subprocess.run(("gsutil cat {} | samtools view -H | grep ^@RG | tr '\t' '\n' | grep -m1 '^SM:' | cut -d ':' -f 2").format(file_path),
                                     shell=True, capture_output=True, encoding="utf-8").stdout
             f.write(file_path + "\t" + file_no_ext + "\t" + sample + "\n")
